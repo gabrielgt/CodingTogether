484ffc136a0ae403e7ebb1e06c6ac22cf5fedcba
Gabriel GT <gabriel@gabriels-Mac-Pro.local>
2013-02-17 01:50:48 +0100

    New view with Set Cards Game

---
+++ Matchismo/Matchismo/PlayingCardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardGameViewController.h
+//  Matchismo
+//
+//  Created by gabriel on 13/02/13.
+//  Copyright (c) 2013 Zetalogic. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface PlayingCardGameViewController : CardGameViewController
+
+@end
